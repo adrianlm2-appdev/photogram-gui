a2e311866ea1f5508f3b73be36c9449255576412
adrianlm2 <adrianl0@chicagobooth.edu>
2022-02-22 15:36:33 +0000

    hi

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -25,8 +25,22 @@ class PhotosController < ApplicationController
   end
 
   def create
+  #parameters: {"input_image"=>"a", "input_caption"=>"b", "input_owner_id"=>"c"}
 
-  render(:template => "photo_templates/create.html.erb")
+  input_image = params.fetch("input_image")
+  input_caption = params.fetch("input_caption")
+  input_owner_id = params.fetch("input_owner_id")
+
+  a_new_photo = Photo.new
+
+  a_new_photo.image = input_image
+  a_new_photo.caption = input_caption
+  a_new_photo.owner_id = input_owner_id
+
+  a_new_photo.save
+
+  #render(:template => "photo_templates/create.html.erb")
+  redirect_to("/photos/" + a_new_photo.id.to_s)
   end 
 
 end 
