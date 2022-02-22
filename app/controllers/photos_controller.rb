225dad6e603ecbe1ec119c6f5f75a33c9f7bde2e
adrianlm2 <adrianl0@chicagobooth.edu>
2022-02-22 15:52:50 +0000

    hello

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -43,4 +43,25 @@ class PhotosController < ApplicationController
   redirect_to("/photos/" + a_new_photo.id.to_s)
   end 
 
+  def update
+#Parameters: {"input_image"=>"https://robohash.org/dolorehicincidunt.png?size=300x300&set=set1", "input_caption"=>"hi", "modify_id"=>"777"}
+
+  the_id = params.fetch("modify_id")
+  matching_photos = Photo.where({ :id => the_id })
+  the_photo = matching_photos.at(0)
+
+  input_image = params.fetch("input_image")
+  input_caption = params.fetch("input_caption")
+
+
+
+  the_photo.image = input_image
+  the_photo.caption = input_caption
+
+  the_photo.save
+
+  redirect_to("photos/" + the_photo.id.to_s)
+
+  #render(:template => "photo_templates/update.html.erb")
+  end 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ get("/photos", {:controller => "photos", :action => "index" })
 get("/photos/:path_id", {:controller => "photos", :action => "show" })
 get("/delete_photo/:path_id", {:controller => "photos", :action => "delete"})
 get("/insert_photo", {:controller => "photos", :action => "create"})
+get("/update_photo/:modify_id", {:controller => "photos", :action => "update" })
 end
